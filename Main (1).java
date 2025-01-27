d58702ed317a7cb921d863b4b5cb62a604f5c916
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-01-27 14:11:37 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,11 @@
+public class Animal{
+    String color;
+    int age ;
+    public static void main(String args[]){
+        Animal piyush = new Animal();
+        piyush.color = "white";
+        piyush.age = 20;
+        System.out.println(piyush.color);
+        System.out.println(piyush.age);
+    }
+}
